4e74c03caeb819a6bfb48de7fdaa4de5c213c983
IAmThePX <alphabet.elandkhe@gmail.com>
2026-01-31 09:17:21 +0700

    Adding Test Suites

---
+++ Test Suites/TS_E2E_All.ts
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS_E2E_All</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>10</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>f1e66002-374a-4532-a8e7-c1e4b34bedbb</testSuiteGuid>
+   <testCaseLink>
+      <guid>c2b5cac8-1476-49ed-9a51-89956122f51a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/E2E/TC_E2E_UI_Employee_Training_Assignment</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>1ea9f138-9e72-46cf-962d-3590fbec9980</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/E2E/TC_E2E_API_Employee_Training</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
